6beb2d47ea8cbe10ae01475448acc54df1c3f265
sanfernoronha <sanfernoronha99@gmail.com>
2020-12-23 12:35:43 +0530

    :wrench: edited procfile

--- app.py
+++ app.py
@@ -37,6 +37,6 @@ def check():
 
 
 if __name__ == "__main__":
-    # port = int(os.environ.get("PORT",5000))
-    # app.run(host='0.0.0.0',port=port)
-    app.run(debug=True,use_reloaded=True)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host='0.0.0.0',port=port)
+    

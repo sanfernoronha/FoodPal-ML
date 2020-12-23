d987ec3818a898f10efb533214c1d7799fff5eb4
sanfernoronha <sanfernoronha99@gmail.com>
2020-12-23 10:59:18 +0530

    :wrench: added host

--- app.py
+++ app.py
@@ -37,5 +37,5 @@ def add_headers(response):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT',5000))
-    app.run(port=port)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host='0.0.0.0',port=port)
